91a7530a69b287fd0c77202fbb2d0744342c707e
Galang Piliang <galang.pil@gmail.com>
2018-07-06 02:10:41 +0700

    Adding vuforia plugin

---
+++ Assets/_Galpil/Script/LoadScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour {
+
+	public void GoToScene(int sceneIndex) {
+		SceneManager.LoadScene(sceneIndex);
+	}
+}
---
+++ Assets/_Galpil/Script/ToogleFlash.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ToogleFlash : MonoBehaviour {
+
+	bool flashon = false;
+	
+	public void OnTap() {
+		if (!flashon)
+			SetOn();
+		else
+			SetOff();
+	}
+
+	void SetOn(){
+		CameraDevice.Instance.SetFlashTorchMode(true);
+		flashon=true;
+	}
+
+	void SetOff(){
+		CameraDevice.Instance.SetFlashTorchMode(false);
+		flashon=false;
+	}
+}
